eb187e309903b494c8cc8798e8300996a3f86ba0
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2022-10-06 13:26:56 +0200

    Add project files.

---
+++ Fish.cs
@@ -0,0 +1,70 @@
+﻿namespace CA221006_2
+{
+    internal class Fish
+    {
+        private float _weight;
+        private bool _isWeightSet = false;
+        private bool _predator;
+        private int _swimTop;
+        private int _swimDepth;
+        private string _species;
+
+        public float Weight
+        {
+            get => _weight;
+            set
+            {
+                if (value < .5f)
+                    throw new Exception("egy hal súlya nem lehet kevesebb, mint 0.5 Kg");
+                if (value > 40f)
+                    throw new Exception("egy hal súlya nem lehet több, mint 40 Kg");
+                if (_isWeightSet && (value > _weight * 1.1f || value < _weight * .9f))
+                    throw new Exception("egy hal súlya maximum 10%-al változhat!");
+                else _isWeightSet = true;
+
+                _weight = value;
+            }
+        }
+        public bool Predator
+        {
+            get => _predator;
+            private set
+            {
+                _predator = value;
+            }
+        }
+        public int SwimTop
+        {
+            get => _swimTop;
+            set
+            {
+                _swimTop = value;
+            }
+        }
+        public int SwimDepth
+        {
+            get => _swimDepth;
+            set
+            {
+                _swimDepth = value;
+            }
+        }
+        public string Species
+        {
+            get => _species;
+            set
+            {
+                _species = value;
+            }
+        }
+
+        public Fish(float weight, bool predator, int swimTop, int swimDepth, string species)
+        {
+            Weight = weight;
+            Predator = predator;
+            SwimTop = swimTop;
+            SwimDepth = swimDepth;
+            Species = species;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿namespace CA221006_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            ElvisOperator();
+        }
+
+        private static void ElvisOperator()
+        {
+            // <condition> ? <code if TRUE> : <code if FALSE>
+            //<type> <variable> = <condition> ? <value if TRUE> : <value if FALSE>
+
+            string szin = "zöld";
+            Console.WriteLine(szin == "lila" ? "a kabát színe lila" : "a kabát színe NEM lila");
+
+            //if (szin == "lila")
+            //{
+            //    Console.WriteLine("a kabát színe lila");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("a kabát színe NEM lila");
+            //}
+
+
+            Console.Write("szeretsz élni? ");
+            bool valasz = Console.ReadLine() == "nem" ? false : true;
+
+            //bool valasz;
+            //string valaszString = Console.ReadLine();
+            //if (valaszString == "nem")
+            //{
+            //    valasz = false;
+            //}
+            //else
+            //{
+            //    valasz = true;
+            //}
+            //Console.WriteLine($"valasz értéke: {valasz}");
+
+        }
+
+        private static void Test()
+        {
+            #region tesztek
+
+            #region üres konstruktoros kezdőérték adás
+            //Fish f = new()
+            //{
+            //    Weight = 10f,
+            //};
+            #endregion
+
+            Fish f = new(
+                weight: 25f,
+                predator: true,
+                swimTop: 10,
+                swimDepth: 10,
+                species: "Keszeg");
+
+            f.Weight -= .1f;
+            //f.Predator = false;
+
+            Console.WriteLine($"A {(f.Predator ? "ragadozó" : "növényevő")} hal súlya {f.Weight:00.00} Kg");
+            #endregion
+        }
+
+        
+    }
+}
